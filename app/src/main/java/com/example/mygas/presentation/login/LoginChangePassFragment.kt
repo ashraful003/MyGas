c4a84abe942d0e742c0184d57c19d413b2e0c054
ashraful003 <ashraful10178@gmail.com>
2024-01-22 13:32:56 +0800

    Design Login Change Password Fragment AA MG-1007

--- app/src/main/java/com/example/mygas/presentation/login/LoginChangePassFragment.kt
+++ app/src/main/java/com/example/mygas/presentation/login/LoginChangePassFragment.kt
@@ -5,17 +5,78 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.mygas.R
+import com.example.mygas.databinding.FragmentLoginChangePassBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
 
 class LoginChangePassFragment : Fragment() {
+    val actionSignIn =
+        Navigation.createNavigateOnClickListener(R.id.action_loginChangePassFragment_to_loginInputFragment)
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginChangePassBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_change_pass, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login_change_pass, container, false)
+        binding.model = this
+        isEnableSubmitButton(false)
+        val passwordStream = RxTextView.textChanges(binding.newPasswordEt)
+            .skipInitialValue()
+            .map { password ->
+                password.isEmpty()
+            }
+
+        val confirmPassStream = Observable.merge(
+            RxTextView.textChanges(binding.newPasswordEt)
+                .skipInitialValue()
+                .map { password ->
+                    password.toString() != binding.newConfirmPasswordEt.text.toString()
+                },
+            RxTextView.textChanges(binding.newConfirmPasswordEt)
+                .skipInitialValue()
+                .map { confirmPass ->
+                    confirmPass.toString() != binding.newPasswordEt.text.toString()
+                })
+        val invalidFiledStream = Observable.combineLatest(
+            passwordStream,
+            confirmPassStream
+        ) { passwordInvalid: Boolean, confirmPassInvalid: Boolean ->
+            !passwordInvalid && !confirmPassInvalid
+        }
+        invalidFiledStream.subscribe { isValid ->
+            isEnableSubmitButton(isValid)
+        }
+
+        binding.backIv.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        return binding.root
     }
 
+    private fun isEnableSubmitButton(isEnable:Boolean?){
+        if (isEnable == true){
+            binding.btnDone.isEnabled = true
+            binding.btnDone.backgroundTintList = ContextCompat.getColorStateList(requireActivity(), R.color.red_500)
+        }
+        else{
+            binding.btnDone.isEnabled = false
+            binding.btnDone.backgroundTintList = ContextCompat.getColorStateList(requireActivity(),R.color.red_500_shadow)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+    }
 
 }
